0cd19c4cfc113c9e22d8d0cff3135a436ff1e14e
Abhishek V Tatachar <abhishektatachar@gmail.com>
2023-11-06 00:48:38 +0530

    Exposing the apis for issue handling

--- backend/app.py
+++ backend/app.py
@@ -71,7 +71,7 @@ def create_issue():
     try:
         project_id = request.form["project_id"]
         reporter_id = request.form["reporter_id"]
-        assignee_id = request.form["assignee_id"]
+        assignee_id = request.form["assignee_id"] 
         issue_title = request.form["issue_title"]
         issue_description = request.form["issue_description"]
         priority = request.form["priority"]
@@ -85,9 +85,119 @@ def create_issue():
         sql_query = f"INSERT INTO issue (issue_id, project_id, issue_title, issue_description, reporter_id, assignee_id, priority, story_points) VALUES ('{issue_id}', '{project_id}', '{issue_title}', '{issue_description}', '{reporter_id}', '{assignee_id}', '{priority}', '{story_points}')"
         cursor.execute(sql_query)
         conn.commit()
+        id_exists = 1
+        while id_exists:
+            assignment_id = random.randint(1000000, 9999999)
+            sql_query = f"SELECT COUNT(*) FROM assignment where assignment_id = '{assignment_id}'"
+            cursor.execute(sql_query)
+            id_exists = cursor.fetchone()[0]
+        assignment_query = f"INSERT INTO assignment (assignment_id, assignee_id, issue_id) VALUES ('{assignment_id}', '{assignee_id}', '{issue_id}')"
+        cursor.execute(assignment_query)
+        conn.commit()
         return "Success"
     except Exception as e:
         return f"Failure. The following error occured with the database: {str(e)}"
+    
+@app.route("/update_issue", methods=["POST"])
+def create_issue():
+    issue_id = str(request.form["issue_id"])
+    update_fields = []
+    update_values = []
+    sql_query = f"SELECT * FROM ISSUES WHERE issue_id = {issue_id}"
+    cursor.execute(sql_query)
+    result = cursor.fetchone()[0]
+    try:
+        if result:
+            for i in range(len(update_fields)):
+                update_field = update_fields[i]
+                update_value = update_values[i]
+                update_query = f"UPDATE TABLE issue SET {update_field} = '{update_value}' WHERE issue_id = '{issue_id}'"
+                cursor.execute(update_query)
+                conn.commit()
+            return "Success"
+        else:
+            return "Failure. The mentioned issue does not exist."
+    except Exception as e:
+        return f"Failure. The following error ouccured: {str(e)}"
+
+@app.route("delete_issue", methods=["POST"])
+def delete_issue():
+    issue_id = str(request.form["issue_id"])
+    sql_query = f"SELECT * FROM ISSUES WHERE issue_id = {issue_id}"
+    cursor.execute(sql_query)
+    result = cursor.fetchone()[0]
+    try:
+        if result:
+            delete_issue = f"DELETE FROM issue WHERE issue_id = {issue_id}"
+            cursor.execute(delete_issue)
+            conn.commit()
+            return "Success"
+        else:
+            return "Failed. The mentioned issue does not exist."
+    except Exception as e:
+        return f"Failed. The following error occured. {str(e)}"
+
+@app.route("update_priority", methods=["POST"])
+def update_priority():
+    issue_id = str(request.form["issue_id"])
+    priority = str(request.form["priority"])
+    sql_query = f"SELECT * FROM ISSUES WHERE issue_id = {issue_id}"
+    cursor.execute(sql_query)
+    result = cursor.fetchone()[0]
+    try:
+        if result:
+            update_prio = f"UPDATE TABLE issue SET priority = '{priority}' WHERE issue_id = '{issue_id}'"
+            cursor.execute(update_prio)
+            conn.commit()
+            return "Success"
+        else:
+            return "Failed. The mentioned issue does not exist"
+    except Exception as e:
+        return f"Failed. The following error occoured: {str(e)}"
+
+@app.route("update_story_points", methods=["POST"])
+def update_story_priority():
+    issue_id = str(request.form["issue_id"])
+    story_points = str(request.form["story_points"])
+    sql_query = f"SELECT * FROM ISSUES WHERE issue_id = {issue_id}"
+    cursor.execute(sql_query)
+    result = cursor.fetchone()[0]
+    try:
+        if result:
+            update_prio = f"UPDATE TABLE issue SET story_points = '{story_points}' WHERE issue_id = '{issue_id}'"
+            cursor.execute(update_prio)
+            conn.commit()
+            return "Success"
+        else:
+            return "Failed. The mentioned issue does not exist"
+    except Exception as e:
+        return f"Failed. The following error occoured: {str(e)}"
+
+@app.route("scrum_update", methods = ["POST"])
+def scrum_update():
+    scrum_update = str(request.form["scrum_update"])
+    #TODO: Process the scrum update (AI agent work).
+
+@app.route("assign_issue", methods = ["POST"])
+def assign_issue():
+    try:
+        issue_id = request.form["issue_id"]
+        assignee_id = request.form["assignee_id"]
+        issue_exists = f"SELECT count(*) FROM issue WHERE issue_id = '{issue_id}'"
+        cursor.execute(issue_exists)
+        result = cursor.fetchone()[0]
+        if result:
+            update_issue = f"UPDATE table issue set assignee_id = '{assignee_id}' where issue_id = '{issue_id}'"
+            cursor.execute(update_issue)
+            conn.commit()
+            update_assignment = f"UPDATE table assignment set assignee_id = '{assignee_id}' where issue_id = '{issue_id}'"
+            cursor.execute(update_assignment)
+            conn.commit()
+        else:
+            return "Failed. The mentioned issue does not exist."
+    except Exception as e:
+        return f"Failed. The following error occured with the database. {str(e)}"
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
     
