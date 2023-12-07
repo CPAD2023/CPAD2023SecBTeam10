21495c048c2d32d47fa1303eca2c1c3b04ba45fe
Abhishek V Tatachar <abhishektatachar@gmail.com>
2023-12-07 10:58:04 +0530

    temp_commit

---
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/i527379/Documents/mtech_lab/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/i527379/Documents/MTech Sem3/Assignments/cpad/scrumbot/CPAD2023SecBTeam10/frontend"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/i527379/Documents/MTech Sem3/Assignments/cpad/scrumbot/CPAD2023SecBTeam10/frontend/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC81NGE3MTQ1MzAzZjBkZDlkMGY5MzQyNGEyZTEyNGViNGFiZWY1MDkxLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/i527379/Documents/MTech Sem3/Assignments/cpad/scrumbot/CPAD2023SecBTeam10/frontend/.dart_tool/package_config.json"
